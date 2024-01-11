7c87692632af85754f60908b020d4bcc6e312b40
samkasperowicz <148405744+samkasperowicz@users.noreply.github.com>
2024-01-11 13:11:05 -0600

    Update to account for conversion to the same currency

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,10 +9,15 @@ let inpt2 = inputs[1];
 resultButton.addEventListener("click", doConversion);
 
 async function doConversion() {
-    let freq = await fetch("https://api.frankfurter.app/latest?from=" + sel1.value +"&to=" + sel2.value);
-    let json = await freq.json();
-    let rate = json.rates[sel2.value];
-    let result = parseFloat(inpt1.value) * parseFloat(rate);
-    inpt2.value = Number.parseFloat(result).toFixed(2);
+    if (sel1.value == sel2.value) {
+        inpt2.value = inpt1.value;
+    }
+    else {
+        let freq = await fetch("https://api.frankfurter.app/latest?from=" + sel1.value +"&to=" + sel2.value);
+        let json = await freq.json();
+        let rate = json.rates[sel2.value];
+        let result = parseFloat(inpt1.value) * parseFloat(rate);
+        inpt2.value = Number.parseFloat(result).toFixed(2);
+    }
 }
 
